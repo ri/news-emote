c6105904a84da3dc0388547dff17abe4bf368913
Ri <me@ri.id.au>
2017-04-13 12:22:19 +1000

    update app structure

--- src/App/App copy.js
+++ src/App/App copy.js
@@ -1,16 +1,15 @@
-import * as styles from './App.scss';
+import * as styles from './App.scss'
 
 export class App {
-
   constructor(options) {
     this.props = {
       dom: null,
       rest: 'test rest',
       ...options // ES6: rest properties
-    };
-    console.log(this.props);
+    }
+    console.log(this.props)
 
-    this.props.dom.innerHTML = 'Loading from github...';
+    this.props.dom.innerHTML = 'Loading from github...'
   }
 
   render(json) {
@@ -19,28 +18,27 @@ export class App {
         const row = [
           `<a href="${item.html_url}" target="_blank">${item.full_name}</a>`,
           item.score
-        ];
+        ]
 
-        return `<tr>${row.map(r => `<td>${r}</td>`).join('')}</tr>`;
-      }).join('');
+        return `<tr>${row.map(r => `<td>${r}</td>`).join('')}</tr>`
+      }).join('')
 
       // table
-      const table = document.createElement('table');
-      table.classList.add(styles.table);
+      const table = document.createElement('table')
+      table.classList.add(styles.table)
 
-      const caption = '<caption>Popular ES6 projects at github.com</caption>';
+      const caption = '<caption>Popular ES6 projects at github.com</caption>'
 
       // thead
-      const thead = `<thead><tr>${['name', 'correlation'].map(t => `<th>${t}</th>`).join('')}</tr></thead>`;
+      const thead = `<thead><tr>${['name', 'correlation'].map(t => `<th>${t}</th>`).join('')}</tr></thead>`
 
       // tbody
-      const tbody = `<tbody>${trs}</tbody>`;
+      const tbody = `<tbody>${trs}</tbody>`
 
-      table.innerHTML = `${caption}${thead}${tbody}`;
+      table.innerHTML = `${caption}${thead}${tbody}`
 
-      this.props.dom.innerHTML = '';
-      this.props.dom.appendChild(table);
+      this.props.dom.innerHTML = ''
+      this.props.dom.appendChild(table)
     }
   }
-
 }
--- src/App/Headlines.js
+++ src/App/Headlines.js
@@ -1,9 +1,8 @@
 import * as d3 from 'd3'
 
 export class Headlines {
-
   constructor(data) {
-    this.data = data["news-data"]
+    this.data = data['news-data']
     this.data.forEach((d) => {
       d.counts = []
       d.counts[0] = { pos: 0, neg: 0, neu: 0, total: 0 }
@@ -31,13 +30,8 @@ export class Headlines {
     //   // d3.select("#feeds").append("div").style("background-color", cScale(mean))
 
     // })
-    this.cScale = d3.scalePow(2).domain([-1, -0.5, 0, 0.5, 1]).range(['#d7191c','#fdae61','#ffffbf','#a6d96a','#1a9641'])
-    // this.lineY = d3.scaleLinear().range([60, 0])
-    // this.lineX = d3.scaleLinear().domain([0, 0]).range([0, 200])
-    // this.posLine = this.lineGen("pos")
-    // this.negLine = this.lineGen("neg")
-    // this.neuLine = this.lineGen("neu")
-    this.contain = d3.select("#feeds")
+    this.cScale = d3.scalePow(2).domain([-1, -0.5, 0, 0.5, 1]).range(['#d7191c', '#fdae61', '#ffffbf', '#a6d96a', '#1a9641'])
+    this.contain = d3.select('#feeds')
 
     this.draw()
   }
@@ -45,99 +39,64 @@ export class Headlines {
   draw() {
     var self = this
 
-    this.newsFeeds = this.contain.selectAll("div.news-feed")
+    this.newsFeeds = this.contain.selectAll('div.news-feed')
       .data(this.data)
       .enter()
-      .append("div")
-      .attr("class", "news-feed")
+      .append('div')
+      .attr('class', 'news-feed')
 
-    this.lines = this.newsFeeds.append("svg")
-      .attr("class", "sentiment-lines")
-      .style("width", "100%")
-      .style("height", "5")
+    this.lines = this.newsFeeds.append('svg')
+      .attr('class', 'sentiment-lines')
+      .style('width', '100%')
+      .style('height', '5')
 
-    this.newsFeeds.append("div")
-      .attr("class", "title")
+    this.newsFeeds.append('div')
+      .attr('class', 'title')
       .text((d) => d.name)
 
-    this.lines.append("rect")
+    this.lines.append('rect')
       .datum((d) => d.counts)
-      .attr("class", "pos-line")
-      .attr("height", 5)
-      .attr("width", `${100/3}%`)
+      .attr('class', 'pos-line')
+      .attr('height', 5)
+      .attr('width', `${100 / 3}%`)
       // .style("width", "calc(100%  /3);")
 
-    this.lines.append("rect")
+    this.lines.append('rect')
       .datum((d) => d.counts)
-      .attr("class", "neg-line")
-      .attr("height", 5)
-      .attr("x", `${100/3}%`)
-      .attr("width", `${100/3}%`)
-      // .style("width", "calc(100%  /3);")
+      .attr('class', 'neg-line')
+      .attr('height', 5)
+      .attr('x', `${100 / 3}%`)
+      .attr('width', `${100 / 3}%`)
 
-    this.lines.append("rect")
+    this.lines.append('rect')
       .datum((d) => d.counts)
-      .attr("class", "neu-line")
-      .attr("height", 5)
-      .attr("x", `${100/3 * 2}%`)
-      .attr("width", `${100/3}%`)
-      // .style("width", "calc(100%  /3);")
-    // this.lines = this.newsFeeds.append("svg")
-    //   .attr("class", "sentiment-lines")
-    //   .attr("width", "100%")
-    //   .attr("height", 100)
+      .attr('class', 'neu-line')
+      .attr('height', 5)
+      .attr('x', `${100 / 3 * 2}%`)
+      .attr('width', `${100 / 3}%`)
 
+    this.newsFeeds.append('div')
+      .attr('class', 'headlines')
+      .each(function(d, i) { self.createHeadlines(d, this, i) })
 
-    // this.lines.append("path")
-    //   .datum((d) => d.counts)
-    //   .attr("stroke", "green")
-    //   .attr("fill", "none")
-    //   .attr("class", "pos-line")
-    //   .attr("d", this.posLine)
-
-    // this.lines.append("path")
-    //   .datum((d) => d.counts)
-    //   .attr("stroke", "red")
-    //   .attr("fill", "none")
-    //   .attr("class", "neg-line")
-    //   .attr("d", this.negLine)
-
-    // this.lines.append("path")
-    //   .datum((d) => d.counts)
-    //   .attr("stroke", "grey")
-    //   .attr("fill", "none")
-    //   .attr("class", "neu-line")
-    //   .attr("d", this.neuLine)
-
-    this.newsFeeds.append("div")
-      .attr("class", "headlines")
-      .each(function(d,i) { self.createHeadlines(d, this, i)})
-
-    d3.select("#show-all").on("click", () => this.filterBy(this.noFilter))
-    d3.select("#hide-neutral").on("click", () => this.filterBy(this.noNeutralFilter))
-    d3.select("#all-bad").on("click", () => this.filterBy(this.negFilter))
-    d3.select("#all-good").on("click", () => this.filterBy(this.posFilter))
-
+    d3.select('#show-all').on('click', () => this.filterBy(this.noFilter))
+    d3.select('#hide-neutral').on('click', () => this.filterBy(this.noNeutralFilter))
+    d3.select('#all-bad').on('click', () => this.filterBy(this.negFilter))
+    d3.select('#all-good').on('click', () => this.filterBy(this.posFilter))
   }
-
-  // lineGen(key) {
-  //   var line = d3.line()
-  //     .x((d,i) => this.lineX(i))
-  //     .y((d) => this.lineY(d[key]/d.total ? d[key]/d.total : 0))
-  //     .curve(d3.curveCatmullRom)
-  //   return line
-  // }
 
   filterBy(filter) {
     this.currentFilter = filter
-    this.contain.selectAll(".headline")
-      .style("display", (d) => filter(d) ? null : "none")
+    this.contain.selectAll('.headline')
+      .style('display', (d) => filter(d) ? null : 'none')
   }
 
   genTag(entries, tag, text) {
     var tagged = text
-    if(entries.length >0) {
-      entries.forEach((w) => tagged = text.replace(w.word, `<span class="${tag}">${w.word}</span>`))
+    if (entries.length > 0) {
+      entries.forEach(function(w) {
+        tagged = text.replace(w.word, `<span class="${tag}">${w.word}</span>`)
+      })
     }
     return tagged
   }
@@ -152,7 +111,7 @@ export class Headlines {
   }
 
   sentimentCat(d) {
-    return this.posFilter(d) ? "pos" : this.negFilter(d) ? "neg" : "neu"
+    return this.posFilter(d) ? 'pos' : this.negFilter(d) ? 'neg' : 'neu'
   }
 
   createHeadlines(data, el, index) {
@@ -173,45 +132,46 @@ export class Headlines {
           counts.total++
           data.counts.push(counts)
 
-          d3.select(el).insert("div", ":first-child")
+          d3.select(el).insert('div', ':first-child')
             .datum(d)
-            .attr("class", "headline")
+            .attr('class', 'headline')
             .html((d) => this.emText(d))
-            .style("background-color", (d) => this.cScale(d.sentiment.compound))
-            .style("margin-top", function() { return `-${d3.select(this).node().offsetHeight}px`})
-            .style("display", (d) => this.currentFilter(d) ? null : "none")
+            .style('background-color', (d) => this.cScale(d.sentiment.compound))
+            .style('margin-top', function() { return `-${d3.select(this).node().offsetHeight}px` })
+            .style('display', (d) => this.currentFilter(d) ? null : 'none')
             .transition()
             .delay(200)
-            .style("margin-top", "0px")
+            .style('margin-top', '0px')
 
           var lines = d3.select(el.parentNode)
 
-          lines.select(".sentiment-lines .pos-line")
+          lines.select('.sentiment-lines .pos-line')
             // .datum((d) => data.counts)
             .transition()
-            .attr("width", `${counts.pos/counts.total * 100}%`)
+            .attr('width', `${counts.pos / counts.total * 100}%`)
 
-          lines.select(".sentiment-lines .neg-line")
+          lines.select('.sentiment-lines .neg-line')
             // .datum((d) => data.counts)
             .transition()
-            .attr("x", `${counts.pos/counts.total * 100}%`)
-            .attr("width", `${counts.neg/counts.total * 100}%`)
+            .attr('x', `${counts.pos / counts.total * 100}%`)
+            .attr('width', `${counts.neg / counts.total * 100}%`)
 
-          lines.select(".sentiment-lines .neu-line")
+          lines.select('.sentiment-lines .neu-line')
             // .datum((d) => data.counts)
             .transition()
-            .attr("x", `${(counts.pos + counts.neg)/counts.total * 100}%`)
-            .attr("width", `${counts.neu/counts.total * 100}%`)
+            .attr('x', `${(counts.pos + counts.neg) / counts.total * 100}%`)
+            .attr('width', `${counts.neu / counts.total * 100}%`)
 
           // if (this.currentIndex < curr) { this.lineX.domain([0, counts.total])}
           this.currentIndex = curr++
+        } else {
+          d3.select(el).insert('div', ':first-child')
+            .attr('class', 'headlines-end')
+            .text('End of feed')
 
-        } else { t.stop() }
+          t.stop()
+        }
       }, 2400)
     }, index * 400)
-
-
-      // .style("background-color", (d) => cScale(d.sentiment.compound))
   }
 }
-
