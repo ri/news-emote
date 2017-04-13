e1a5e4b7c80fcaea78ac2db7755d88274232a286
Ri <me@ri.id.au>
2017-04-13 12:23:55 +1000

    update bootstrap script

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,38 +1,48 @@
 // global css
-import './theme/theme.scss';
+import './theme/theme.scss'
 
 // classes you want to use immediately
-import {App} from './App';
-import {Http} from './Http';
-
+import {App} from './App'
+import reqwest from 'reqwest'
+import queryString from 'query-string'
+import './vendor/scrollsnap-polyfill.bundled'
 /**
  * entrance code for SPA
  */
 function main() {
-  document.title = 'Loading...';
-  var dom =  document.querySelector('.content')
+  document.title = 'Loading...'
+  var dom = document.querySelector('.content')
+  var hash = queryString.parse(location.search)
+  var loc = 'au'
+
+  if (hash.loc === 'us') {
+    loc = 'us'
+  }
 
   const app = new App({
-    dom: dom,
-    prop1: 'prop1',
-    prop2: 'prop2',
-    prop3: 'prop3'
-  });
+    dom: dom
+  })
+
+  reqwest({
+    url: `https://s3.amazonaws.com/data.newsemote/${loc}/latest.json`,
+    type: 'json',
+    crossOrigin: true,
+    success: (resp) => app.render(resp)
+  })
 
   // we can make requests to multiple domains, check out proxy/rules.js
-  const http = new Http();
 
   // send request to github.com
-  http.get('https://s3.amazonaws.com/data.newsemote/us/latest.json').then((res) => {
-    const data = JSON.parse(res);
-    app.render(data);
-    document.title = 'App Started';
-  });
+  // http.get('https://s3.amazonaws.com/data.newsemote/us/latest.json').then((res) => {
+  //   const data = JSON.parse(res)
+  //   app.render(data)
+  //   document.title = 'App Started'
+  // })
 
-  // send request to npmjs.org
-  http.get('/node-1').then((json) => console.log('From npmjs.org: ', json));
+  // // send request to npmjs.org
+  // http.get('/node-1').then((json) => console.log('From npmjs.org: ', json))
 
-  console.log([1, 2, 3, 4].includes(1)); // ES7: Array.prototype.includes
+  // console.log([1, 2, 3, 4].includes(1)) // ES7: Array.prototype.includes
 }
 
-document.addEventListener('DOMContentLoaded', main);
+document.addEventListener('DOMContentLoaded', main)
